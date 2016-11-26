2e6810e7a771092f03fafba819d2a9bdedff0a93
root_ <kingfredomzz@gmail.com>
2016-11-26 12:42:59 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>مدونة يوسف المعولي</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/main_style.css">
+        <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">
+        <script src="vendor/bootstrap/js/bootstrap.js" ></script>
+    </head>
+
+</html>
